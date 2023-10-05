645b6121cd52eff863976a1c42a4a994513d49ff
abinayaarjunan <abiharsha2000@gmail.com>
2023-10-05 14:48:06 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import SignUp from './components/user/SignUp';
+import Signin from './components/user/Signin';
+import ForgotPassword from './components/user/ForgotPassword';
+import ResetPassword from './components/user/ResetPassword';
+import ActivationPage from './components/user/ActivationPage';
+import Dashboard from './components/url/Dashboard';
+import GenerateUrls from './components/url/GenerateUrls';
+import ViewUrls from './components/url/ViewUrls';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/forgot_password' element={<ForgotPassword />} />
+          <Route path='/reset_password/:token' element={<ResetPassword />} />
+          <Route path='/activation_page/:token' element={<ActivationPage />} />
+          <Route path='/dashboard/:id' element={<Dashboard />} />
+          <Route path='/generate_url/:id' element={<GenerateUrls />} />
+          <Route path='/view_url/:id' element={<ViewUrls />} />
+          <Route path="/" element={<Navigate to="/signin" replace />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
